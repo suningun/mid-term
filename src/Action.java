a7a4b4e1d4a728d68bd98bc7f6947bb89edbd088
mengkheang-seng <2025422seng@aupp.edu.kh>
2026-03-05 09:54:10 +0700

    action

---
+++ src/Action.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Action {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Welcome to library management system!\n");
+        System.out.print("Type a number to proceed: \n");
+        System.out.print("1: Book Management\n" +
+                "2: Member Management\n" +
+                "3: Borrow Book\n" +
+                "4: Return Book\n" +
+                "5: Overdue List\n" +
+                "6: Logout\n" +
+                "7: Exit");
+        System.out.println ("Enter your choice: ");
+        int action = scanner.nextInt();
+
+
+
+
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("THis is for midterm");
-            i++;
-        }
 
     }
 }
